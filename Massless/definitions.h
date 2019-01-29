8b8aca3572c53740af7ca385ed14b5691aaf0b15
GrapeKing <46871864+GrapeKing@users.noreply.github.com>
2019-01-29 10:15:37 +0000

    Add files via upload

---
+++ Massless/definitions.h
@@ -0,0 +1,28 @@
+#define  PHYSICS                        HD
+#define  DIMENSIONS                     1
+#define  COMPONENTS                     1
+#define  GEOMETRY                       SPHERICAL
+#define  BODY_FORCE                     POTENTIAL
+#define  FORCED_TURB                    NO
+#define  COOLING                        NO
+#define  RECONSTRUCTION                 LINEAR
+#define  TIME_STEPPING                  RK2
+#define  DIMENSIONAL_SPLITTING          NO
+#define  NTRACER                        0
+#define  USER_DEF_PARAMETERS            0
+
+/* -- physics dependent declarations -- */
+
+#define  EOS                            ISOTHERMAL
+#define  ENTROPY_SWITCH                 NO
+#define  THERMAL_CONDUCTION             NO
+#define  VISCOSITY                      NO
+#define  ROTATING_FRAME                 NO
+
+/* -- user-defined parameters (labels) -- */
+
+
+/* [Beg] user-defined constants (do not change this line) */
+
+
+/* [End] user-defined constants (do not change this line) */
