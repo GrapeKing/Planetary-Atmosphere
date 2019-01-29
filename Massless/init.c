5a23421100e0c81e3725b150a06e4c34dd3c61a2
William Béthune <bethunew@gmail.com>
2019-01-29 13:50:35 +0000

    suggested modifications for analysis

--- Massless/init.c
+++ Massless/init.c
@@ -43,18 +43,33 @@ void InitDomain (Data *d, Grid *grid)
 
 void Analysis (const Data *d, Grid *grid)
 {
-  double  *x1 = grid->x[IDIR];
+  // Will@Dao:
   int i,j,k;
-  char fname[512];
   FILE *fp;
-  sprintf (fname, "%s/density.txt",RuntimeGet()->output_dir);
-  fp = fopen(fname,"a");
+  static int first_call = 1; // static variable, keeps its value between calls of the function
+  
+  if (first_call == 1) {
+    // Initializing the output files
+    first_call = 0; // now zero for every subsequent call
+    fp = fopen("density.txt","a");
+    fprintf(fp,"%.4e ",g_domBeg[0]); // coordinate of the inner boundary
+    DOM_LOOP(k,j,i){
+      fprintf(fp,"%.4e ",grid->xr[IDIR][i]); // coordinate of every cell boundary
+    }
+    fprintf(fp,"\n"); // new line
+    fclose(fp);
+  }
+
+  // exporting data (you can repeat for the velocity field)
+  fp = fopen("density.txt","a");
+  fprintf(fp,"%.4e ",g_time); // current time
   DOM_LOOP(k,j,i){
-    fprintf(fp,"%.4e ",d->Vc[RHO][k][j][i]);
+    fprintf(fp,"%.4e ",d->Vc[RHO][k][j][i]); // density row
   }
   fprintf(fp,"\n");
   fclose(fp);
-  
+
+  /*
   //I don't know how to execute it only once so I let it run anyway:P
   //Also I did not do this in init because it seems to loop over more cells than the DOM_LOOP command
   sprintf (fname, "%s/coordinate.txt",RuntimeGet()->output_dir);
@@ -63,6 +78,6 @@ void Analysis (const Data *d, Grid *grid)
     fprintf(fp,"%.4e ",x1[i]);
   }
   fclose(fp);
-
-  printf("lol");
+  */
+  
 }
