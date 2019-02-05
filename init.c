633ee93face59de99d3d9d8f91b7a72f90baff24
William Béthune <bethunew@gmail.com>
2019-02-05 17:19:36 +0000

    commented init.c

--- init.c
+++ init.c
@@ -8,7 +8,7 @@ int num_div = 0;
 void Init (double *v, double x1, double x2, double x3)
 {
   // Set initial conditions
-  v[RHO] = g_inputParam[RHOINF];
+  v[RHO] = g_inputParam[RHOINF]; //Will: you may want to start with the analytic hydrostatic solution
   v[VX1] = 0.0;
 
   // Set global sound speed
@@ -30,9 +30,8 @@ void InitDomain (Data *d, Grid *grid) {
   static int first_call = 1;
   if (first_call == 1) {
     first_call = 0;
-    num_div = grid->np_tot[IDIR];
-    mass_gas = (double *) malloc(num_div*sizeof(double));
-    tot_Mg = (double *) malloc(num_div*sizeof(double));
+    mass_gas = (double *) malloc(grid->np_tot[IDIR]*sizeof(double)); //Will: removed the 'num_div' thing
+    tot_Mg = (double *) malloc(grid->np_tot[IDIR]*sizeof(double));
   }
 }
 
@@ -47,14 +46,18 @@ void UserDefBoundary (const Data *d, RBox *box, int side, Grid *grid) {
     BOX_LOOP(box,k,j,i){
       d->Vc[RHO][k][j][i] = g_inputParam[RHOINF];
       //If the following is added I get oscillations:
+      //Will: you still need to impose something; here you are extrapolating VX1 with a constant value,
+      //but if the gas wants to fall, then there is a good chance that the velocity profile will not be flat,
+      //for example with rho=constant, the velocity will tend to vary as 1/r^2 (div(rho.v)=0, no sound waves);
+      //maybe try a linear extrapolation? Otherwise, are the oscillations sustained/amplified, or damped over time?
       //d->Vc[VX1][k][j][i] = d->Vc[VX1][k][j][IEND];
     }
   }
 
   if (side == X1_BEG) {
       BOX_LOOP(box,k,j,i){
-  d->Vc[RHO][k][j][i] = +d->Vc[RHO][k][j][2*IBEG-1-i];
-	d->Vc[VX1][k][j][i] = -d->Vc[VX1][k][j][2*IBEG-1-i];
+	d->Vc[RHO][k][j][i] = +d->Vc[RHO][k][j][2*IBEG-1-i]; //Will: symmetrizing the density, 
+	d->Vc[VX1][k][j][i] = -d->Vc[VX1][k][j][2*IBEG-1-i]; // anti-symmetrizing the velocity --> no mass flux
     }
   }
 
@@ -65,7 +68,7 @@ void UserDefBoundary (const Data *d, RBox *box, int side, Grid *grid) {
       mass_gas[i] = d->Vc[RHO][k][j][i]*dV; 
 
       if (i == IBEG) {
-        tot_Mg[i] = mass_gas[i];
+        tot_Mg[i] = mass_gas[i]; 
       } else {
         tot_Mg[i] = mass_gas[i] + tot_Mg[i-1];
       }
@@ -80,14 +83,14 @@ double BodyForcePotential(double x1, double x2, double x3)
 {
   double Mg = 0;
   int temp = 0;
-  double dx = 15.0/128.0;
+  double dx = 15.0/128.0; //Will: ouch, completely problem dependent! Use g_domBeg[IDIR], g_domEnd[IDIR], 
   double x0 = 1.0;
 
   temp = round((x1-x0)/dx);
 
   Mg = tot_Mg[IBEG+temp-1];
-//Here it should definitely be g_inputParam[BONDI]+Mg but the following produces the expected curve:P
-  return -(g_inputParam[BONDI]-Mg)/x1;
+  //Here it should definitely be g_inputParam[BONDI]+Mg but the following produces the expected curve:P
+  return -(g_inputParam[BONDI]-Mg)/x1; //Will: this is not the solution of Poisson's equation!
 }
 
 
